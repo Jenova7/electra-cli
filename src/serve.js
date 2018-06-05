faf687a3ec3aefbec9997a97182d8625094bca7d
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-05 09:47:49 +0200

    serve: add wallet start data retrieving

--- src/serve.js
+++ src/serve.js
@@ -3,8 +3,11 @@ const ElectraJs = require('electra-js')
 const express = require('express')
 const path = require('path')
 
+const download = require('./helpers/download')
+const getGithubReleaseFileInfo = require('./helpers/getGithubReleaseFileInfo')
 const getLogLines = require('./helpers/getLogLines')
 const onSigint = require('./helpers/onSigint')
+const unzip = require('./helpers/unzip')
 
 const PORT = process.env.PORT || 3000
 const VERSION = require(path.resolve(__dirname, '..', `package.json`)).version
@@ -54,8 +57,18 @@ async function run() {
   })
 
   log.warn(`Electra CLI v${VERSION}`)
+
+  const fileName = 'electra-cli-serve-wallet.zip'
+  const filePath = path.resolve(__dirname, '..', 'data', fileName)
+  log.info(`Downloading %s...`, fileName)
+  const fileInfo = await getGithubReleaseFileInfo(fileName)
+  await download(fileInfo.browser_download_url, filePath, fileInfo.size)
+  log.info(`Unzipping %s to %s...`, fileName, electraJs.constants.DAEMON_USER_DIR_PATH)
+  await unzip(filePath, electraJs.constants.DAEMON_USER_DIR_PATH)
+
   // log.info('Starting Express server...')
   // await serve()
+
   log.info('Starting Electra daemon...')
   await electraJs.wallet.startDaemon()
   log.info('Electra daemon started.')
