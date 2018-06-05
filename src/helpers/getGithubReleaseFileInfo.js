443fd4821b539c63fdde1c1dab78d7a48950d915
Ivan Gabriele <ivan.gabriele@gmail.com>
2018-06-05 09:31:39 +0200

    helpers/getGithubReleaseFileInfo: fix Github path

--- src/helpers/getGithubReleaseFileInfo.js
+++ src/helpers/getGithubReleaseFileInfo.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
 modulde.export = async function(fileName) {
-  const assetsApiUrl = (await axios.get(`https://api.github.com/repos/${githubPath}/releases`)).data[0].assets_url
+  const assetsApiUrl = (await axios.get(`https://api.github.com/repos/Electra-project/storage/releases`)).data[0].assets_url
   const asset = (await axios.get(assetsApiUrl)).data.filter(({ name }) => name === fileName)[0]
 }
